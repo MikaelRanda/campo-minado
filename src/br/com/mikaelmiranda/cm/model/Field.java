fd798e6e00657a0c6808e78feaaeaccea37140e5
Kaeldrei <mirandaengcompinfo@gmail.com>
2024-09-16 00:26:40 -0300

    created field attributes

--- src/br/com/mikaelmiranda/cm/model/Field.java
+++ src/br/com/mikaelmiranda/cm/model/Field.java
@@ -1,4 +1,21 @@
 package br.com.mikaelmiranda.cm.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Field {
+
+    private final int linha;
+    private final int coluna;
+
+    private boolean aberto;
+    private boolean minado;
+    private boolean marcado;
+
+    private List<Field> vizinhos = new ArrayList<>();
+
+    Field(int linha, int coluna){
+        this.linha = linha;
+        this.coluna = coluna;
+    }
 }
