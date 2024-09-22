983cb3bcde58e62afef0b2055594854e9d15f789
Kaeldrei <mirandaengcompinfo@gmail.com>
2024-09-22 13:23:25 -0300

    created markers logic test and upgrade the logic relationship

--- src/br/com/mikaelmiranda/cm/model/Field.java
+++ src/br/com/mikaelmiranda/cm/model/Field.java
@@ -68,4 +68,20 @@ public class Field {
     boolean vizinhancaSegura(){
         return vizinhos.stream().noneMatch(v -> v.minado);
     }
+
+    public boolean isMarcado(){
+        return marcado;
+    }
+
+    void minar(){
+        minado = true;
+    }
+
+    public boolean isAberto(){
+        return aberto;
+    }
+
+    public boolean isFechado(){
+        return !isAberto();
+    }
 }
--- test/br/com/mikaelmiranda/cm/model/FieldTest.java
+++ test/br/com/mikaelmiranda/cm/model/FieldTest.java
@@ -1,5 +1,6 @@
 package br.com.mikaelmiranda.cm.model;
 
+import br.com.mikaelmiranda.cm.exception.ExplosaoException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
@@ -53,5 +54,80 @@ public class FieldTest {
         Field vizinho = new Field(1, 1);
         boolean result = field.adicionarVizinhos(vizinho);
         assertFalse(result);
+    }
+
+    @Test
+    void testeValorPadraoAtributoMarcado(){
+        assertFalse(field.isMarcado());
+    }
+
+    @Test
+    void testeAlternarMarcacao(){
+        field.alternarMarcacao();
+        assertTrue(field.isMarcado());
+    }
+
+    @Test
+    void testeAlternarMarcacaoDuasChamadas(){
+        field.alternarMarcacao();
+        field.alternarMarcacao();
+        assertFalse(field.isMarcado());
+    }
+
+    @Test
+    void testeAbrirNaoMinadoNaoMarcado(){
+        assertTrue(field.abrir());
+    }
+
+    @Test
+    void testeAbrirNaoMinadoMarcado(){
+        field.alternarMarcacao();
+        assertFalse(field.abrir());
+    }
+
+    @Test
+    void testeAbrirMinadoMarcado(){
+        field.alternarMarcacao();
+        field.minar();
+        assertFalse(field.abrir());
+    }
+
+    @Test
+    void testeAbrirMinadoNaoMarcado(){
+        field.minar();
+
+        assertThrows(ExplosaoException.class, () -> {
+            field.abrir();
+        });
+    }
+
+    @Test
+    void testeAbrirComVizinhoDefault(){
+        Field campo11 = new Field(1, 1);
+        Field campo22 = new Field(2, 2);
+
+        campo22.adicionarVizinhos(campo11);
+
+        field.adicionarVizinhos(campo22);
+        field.abrir();
+
+        assertTrue(campo22.isAberto() && campo11.isAberto());
+    }
+
+    @Test
+    void testeAbrirComVizinhoMinado(){
+        Field campo11 = new Field(1, 1);
+        Field campo12 = new Field(1, 2);
+
+        campo12.minar();
+
+        Field campo22 = new Field(2, 2);
+        campo22.adicionarVizinhos(campo11);
+        campo22.adicionarVizinhos(campo12);
+
+        field.adicionarVizinhos(campo22);
+        field.abrir();
+
+        assertTrue(campo22.isAberto() && campo12.isFechado());
     }
 }
