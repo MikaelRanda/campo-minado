2aa4a5afae7c75ddff333a42631e061d16b75fd5
Kaeldrei <mirandaengcompinfo@gmail.com>
2024-09-28 23:19:01 -0300

    created close game exception

---
+++ src/br/com/mikaelmiranda/cm/exception/CloseGameException.java
@@ -0,0 +1,9 @@
+package br.com.mikaelmiranda.cm.exception;
+
+import java.io.Serial;
+
+public class CloseGameException extends RuntimeException {
+
+    @Serial
+    private static final long serialVersionUID = 4863569392464877410L;
+}
